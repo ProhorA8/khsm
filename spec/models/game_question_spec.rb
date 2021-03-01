0d2f3aaf714836c34cd56afb1de746da838218c4
ProhorA8 <Procustom4@gmail.com>
2021-03-01 21:53:15 +0300

    add test gamequestion#correct_answer_key

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -26,6 +26,15 @@ RSpec.describe GameQuestion, type: :model do
     end
   end
 
+  it 'correct.level & .text delegates' do
+    expect(game_question.text).to eq(game_question.question.text)
+    expect(game_question.level).to eq(game_question.question.level)
+  end
+
+  it 'correct .correct_answer_key' do
+    expect(game_question.correct_answer_key).to eq('b')
+  end
+
   # help_hash у нас имеет такой формат:
   # {
   #   fifty_fifty: ['a', 'b'], # При использовании подсказски остались варианты a и b
